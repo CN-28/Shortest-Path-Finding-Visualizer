0d59e41b1041ce787629990e28e0e4ef836665a0
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-10-04 22:55:26 +0200

    add functions for drawing board, main loop

--- main.py
+++ main.py
@@ -30,3 +30,48 @@ class Node:
 
     def display(self, win):
         pygame.draw.rect(win, self.color, (self.y, self.x, self.size, self.size))
+    
+
+
+def makeBoard(rows, size):
+    board = [[Node(i, j, size // rows, rows) for j in range(rows)] for i in range(rows)]
+    return board
+
+
+def drawBoard(win, rows, size):
+    spacing = size // rows
+    for i in range(rows):
+        pygame.draw.line(win, grey, (0, i * spacing), (size, i * spacing))
+        for j in range(rows):
+            pygame.draw.line(win, grey, (j * spacing, 0), (j * spacing, size))
+
+
+def draw(win, board, rows, size):
+    win.fill(white)
+    for row in board:
+        for node in row:
+            node.display(win)
+        
+    drawBoard(win, rows, size)
+    pygame.display.update()
+
+
+
+def main(win, size):
+    rows = 60
+    board = makeBoard(rows, size)
+
+    running = True
+    while running:
+        draw(win, board, rows, size)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            
+
+    pygame.quit()
+
+
+
+if __name__ == "__main__":
+    main(window, size)
