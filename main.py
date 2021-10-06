85fc6ff53a96e5028a98292b238d217b3423beb2
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-10-06 14:48:12 +0200

    add clickedPos func. and drawing start, end, walls

--- main.py
+++ main.py
@@ -3,24 +3,18 @@ import bfs, dijkstra, a_star, bellman_ford
 from config import *
 
 class Node:
-    def __init__(self, row, col, size, numberOfRows):
+    def __init__(self, row, col, size):
         self.row = row
         self.col = col
         self.y = row * size
         self.x = col * size
         self.color = white
         self.size = size
-        self.numberOfRows = numberOfRows
     
 
     def display(self, win):
         pygame.draw.rect(win, self.color, (self.y, self.x, self.size, self.size))
     
-
-
-def makeBoard():
-    board = [[Node(i, j, size // rows, rows) for j in range(rows)] for i in range(rows)]
-    return board
 
 
 def drawBoard(win, board):
@@ -36,19 +30,43 @@ def drawBoard(win, board):
 
     pygame.display.update()
 
+
+def clickedPos(pos):
+    spacing = size // rows
+    x, y = pos
+    row, col = x // spacing, y // spacing
+    return row, col
     
+
 
 def main():
     win = pygame.display.set_mode((size, size))
     pygame.display.set_caption("Shortest Path Finding Algorithms")
-    board = makeBoard()
+    board = [[Node(i, j, size // rows) for j in range(rows)] for i in range(rows)]
   
+
+    startNode = None
+    endNode = None
     running = True
     while running:
         drawBoard(win, board)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            
+            if pygame.mouse.get_pressed()[0]:
+                row, col = clickedPos(pygame.mouse.get_pos())
+                node = board[row][col]
+                if not startNode:
+                    startNode = node
+                    startNode.color = yellow
+
+                elif not endNode and node != startNode:
+                    endNode = node
+                    endNode.color = orange
+
+                elif node != endNode and node != startNode:
+                    node.color = black
             
 
     pygame.quit()
