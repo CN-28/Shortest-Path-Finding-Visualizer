74ce73dfb58418d755a3c2cbba28958ef13492fb
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-10-06 14:10:59 +0200

    add config file with global variables

---
+++ config.py
@@ -0,0 +1,11 @@
+rows = 60
+size = 900
+white = 255, 255, 255
+black = 0, 0, 0
+red = 255, 0, 0
+green = 0, 255, 0
+blue = 0, 255, 0
+yellow = 255, 255, 0
+purple = 128, 0, 128
+orange = 255, 165, 0
+grey = 128, 128, 128
--- main.py
+++ main.py
@@ -1,21 +1,6 @@
 import pygame
 import bfs, dijkstra, a_star, bellman_ford
-
-size = 900
-window = pygame.display.set_mode((size, size))
-pygame.display.set_caption("Shortest Path Finding Algorithms")
-
-
-white = 255, 255, 255
-black = 0, 0, 0
-red = 255, 0, 0
-green = 0, 255, 0
-blue = 0, 255, 0
-yellow = 255, 255, 0
-purple = 128, 0, 128
-orange = 255, 165, 0
-grey = 128, 128, 128
-
+from config import *
 
 class Node:
     def __init__(self, row, col, size, numberOfRows):
@@ -33,36 +18,34 @@ class Node:
     
 
 
-def makeBoard(rows, size):
+def makeBoard():
     board = [[Node(i, j, size // rows, rows) for j in range(rows)] for i in range(rows)]
     return board
 
 
-def drawBoard(win, rows, size):
+def drawBoard(win, board):
+    win.fill(white)
+    for row in board:
+        for node in row:
+            node.display(win)
+
     spacing = size // rows
     for i in range(rows):
         pygame.draw.line(win, grey, (0, i * spacing), (size, i * spacing))
         pygame.draw.line(win, grey, (i * spacing, 0), (i * spacing, size))
 
-
-def draw(win, board, rows, size):
-    win.fill(white)
-    for row in board:
-        for node in row:
-            node.display(win)
-        
-    drawBoard(win, rows, size)
     pygame.display.update()
 
+    
 
-
-def main(win, size):
-    rows = 60
-    board = makeBoard(rows, size)
-
+def main():
+    win = pygame.display.set_mode((size, size))
+    pygame.display.set_caption("Shortest Path Finding Algorithms")
+    board = makeBoard()
+  
     running = True
     while running:
-        draw(win, board, rows, size)
+        drawBoard(win, board)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -73,4 +56,4 @@ def main(win, size):
 
 
 if __name__ == "__main__":
-    main(window, size)
+    main()
