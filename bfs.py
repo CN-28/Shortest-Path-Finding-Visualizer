4a7be2b2947e896a46863932d42edc90cd33ac3a
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-10-09 20:41:42 +0200

    optimize bfs implementation

--- bfs.py
+++ bfs.py
@@ -3,44 +3,7 @@ import pygame
 from time import sleep
 from config import *
 
-def BFS(draw, board, start, end):
-    Q = deque()
-    parent = [[None for _ in range(rows)] for _ in range(rows)]
-    Q.append((start.row, start.col))
-    dirs = [(0, 1), (1, 0), (-1, 0), (0, -1)]
-    success = False
-
-    while Q:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-
-        row, col = Q.popleft()
-        if (row, col) == (end.row, end.col):
-            success = True
-            break
-        
-
-        for step_row, step_col in dirs:
-            i, j = row + step_row, col + step_col
-            if 0 <= i < rows and 0 <= j < rows:
-                node = board[i][j]
-                if node.color != black and node.color != red and node.color != yellow:
-                    parent[i][j] = (row, col)
-                    if node != end:
-                        node.color = red
-                        for step_row, step_col in dirs:
-                            k, l = i + step_row, j + step_col
-                            if 0 <= k < rows and 0 <= l < rows:
-                                node = board[k][l]
-                                if node.color == white:
-                                    node.color = blue
-                    Q.append((i, j))
-        draw()
-
-    if not success:
-        return False
-    
+def getPath(draw, board, parent, start, end):
     temp = end.row, end.col
     while temp != None:
         i, j = temp
@@ -53,4 +16,36 @@ def BFS(draw, board, start, end):
     start.color = yellow
     end.color = orange
     draw()
-    return True
+
+
+def BFS(draw, board, start, end):
+    Q = deque()
+    parent = [[None for _ in range(rows)] for _ in range(rows)]
+    Q.append((start.row, start.col))
+    dirs = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+
+    while Q:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        row, col = Q.popleft()
+        curr = board[row][col]
+        if curr == end:
+            getPath(lambda: draw(), board, parent, start, end)
+            return True
+        
+        if curr != start:
+            curr.color = red
+
+        for step_row, step_col in dirs:
+            i, j = row + step_row, col + step_col
+            node = board[i][j]
+            if node.color != white and node != end:
+                continue
+            parent[i][j] = (row, col)
+            if node != end:
+                node.color = blue
+                
+            Q.append((i, j))
+        draw()
